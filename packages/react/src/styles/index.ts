8b9efa0f3611e2e57e93f3a76b93f4cf2f29f28e
Cleidson Oliveira <cleidsonoliveira00@gmail.com>
2023-04-29 22:00:31 -0300

    first commit

---
+++ packages/react/src/styles/index.ts
@@ -0,0 +1,36 @@
+import { createStitches, defaultThemeMap } from '@stitches/react'
+// import {
+//   colors,
+//   fontSizes,
+//   fonts,
+//   fontWeights,
+//   lineHeights,
+//   radii,
+//   space,
+// } from '@ecosystem/tokens'
+
+export const {
+  css,
+  getCssText,
+  globalCss,
+  styled,
+  theme,
+  config,
+  createTheme,
+  keyframes,
+} = createStitches({
+//   themeMap: {
+//     ...defaultThemeMap,
+//     height: 'space',
+//     width: 'space',
+//   },
+//   theme: {
+    // colors,
+    // fonts,
+    // fontSizes,
+    // fontWeights,
+    // lineHeights,
+    // radii,
+    // space,
+//   },
+})
---
+++ packages/tokens/src/colors.ts
@@ -0,0 +1,12 @@
+export const colors = {
+    green100: "#00ff00",
+    green200: "#00ff00",
+    green300: "#00ff00",
+    green400: "#00ff00",
+    green500: "#00ff00",
+    green600: "#00ff00",
+    green700: "#00ff00",
+    green800: "#00ff00",
+    green900: "#00ff00"
+}
+  
