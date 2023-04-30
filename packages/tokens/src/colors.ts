90372e793fdc5310b411bee64448482f937d67e3
Cleidson Oliveira <cleidsonoliveira00@gmail.com>
2023-04-30 18:35:23 -0300

    chore: add green colors tokens

--- packages/tokens/src/colors.ts
+++ packages/tokens/src/colors.ts
@@ -1,12 +1,17 @@
 export const colors = {
-    green100: "#00ff00",
-    green200: "#00ff00",
-    green300: "#00ff00",
-    green400: "#00ff00",
-    green500: "#00ff00",
-    green600: "#00ff00",
-    green700: "#00ff00",
-    green800: "#00ff00",
-    green900: "#00ff00"
+    "green100": "#85ff85",
+    "green200": "#66ff66",
+    "green300": "#43ff43",
+    "green400": "#20ff20",
+    "green500": "#00ff00",
+    "green600": "#00e200",
+    "green700": "#00bc00",
+    "green800": "#00a100",
+    "green900": "#017a01",
+
+    "lemon-green-light": "#c1ff32",
+    "lemon-green-mid": "#adf802",
+    "lemon-green-dark": "#8ac700",
+    "minte-green": "#3EB489",
+    "olive-green": "#808000",
 }
-  
