20333576fc5953410528219f0a2b389cd1b73ed3
Cleidson Oliveira <cleidsonoliveira00@gmail.com>
2023-05-01 21:01:06 -0300

    chore: add variant to button component

--- packages/react/src/components/button.tsx
+++ packages/react/src/components/button.tsx
@@ -2,13 +2,40 @@ import { ComponentProps, ElementType } from "react"
 import { styled } from "../styles"
 
 export const Button = styled("button", {
-    backgroundColor: "$lemon-green-mid",
     borderRadius: "4px",
     fontSize: "13px",
     padding: "10px 15px",
-    "&:hover": {
-        backgroundColor: "$lemon-green-light",
-    },
+    border: "none",
+    color: "white",
+    cursor: "pointer",
+
+    variants: {
+        type: {
+            primary: {
+                backgroundColor: "$minte-green-dark",
+
+                "&:hover": {
+                    backgroundColor: "$minte-green-mid",
+                },
+            },
+            secondary: {
+                backgroundColor: "$green900",
+                
+                "&:hover": {
+                    backgroundColor: "$green700",
+                },
+            },
+        },
+        size: {
+            small: {
+                height: 38,
+            },
+      
+            big: {
+                height: 46,
+            },
+        },
+    }
 });
 
 export interface ButtonProps extends ComponentProps<typeof Button> {
