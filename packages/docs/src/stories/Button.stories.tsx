5cbacf80f428e46cc833300d90ea93eddc1a453f
Cleidson Oliveira <cleidsonoliveira00@gmail.com>
2023-05-01 21:00:41 -0300

    chore: add variant to button storie

--- packages/docs/src/stories/Button.stories.tsx
+++ packages/docs/src/stories/Button.stories.tsx
@@ -3,11 +3,37 @@ import type { Meta, StoryObj } from "@storybook/react"
 import { Button, ButtonProps } from "@ecosystem/react"
 
 export default {
-    title: 'Form/Button',
+    title: "Form/Button",
     component: Button,
     args: {
-      children: 'Send'
-    }    
+      children: "Send",
+      type: "primary",
+      size: "small"
+    },
+    argTypes: {
+      type: {
+        options: ["primary", "secondary"],
+        control: {
+          type: "inline-radio",
+        },
+      },
+      size: {
+        options: ["small", "big"],
+        control: {
+          type: "inline-radio",
+        },
+      }
+    }
 } as Meta<ButtonProps>
   
-export const Primary: StoryObj<ButtonProps> = {}
+export const Primary: StoryObj<ButtonProps> = {
+  args: {
+    type: "primary"
+  },
+}
+
+export const Secondary: StoryObj<ButtonProps> = {
+  args: {
+    type: "secondary"
+  },
+}
