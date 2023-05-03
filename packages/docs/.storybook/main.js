4efe4dadbd1824494d8a421a8ca98e655ced9373
Cleidson Oliveira <cleidsonoliveira00@gmail.com>
2023-05-03 09:56:27 -0300

    fix: add ts and eslint configs to tokens; add project name to storybook/main

--- packages/docs/.storybook/main.js
+++ packages/docs/.storybook/main.js
@@ -18,7 +18,7 @@ module.exports = {
   },
   viteFinal: (config, { configType }) => {
     if (configType === 'PRODUCTION') {
-      config.base = '/design-system-ignite/'
+      config.base = '/ecosystem/'
     }
 
     return config
